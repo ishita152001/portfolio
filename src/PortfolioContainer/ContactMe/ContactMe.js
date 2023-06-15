20aba47761dbc3a73ce455a5029b3c5e9652a45f
ISHITA JAIN <67500673+ishita152001@users.noreply.github.com>
2023-06-15 18:31:50 +0530

    updated contact

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -26,15 +26,6 @@ export default function ContactMe(props) {
   const [banner, setBanner] = useState("");
   const [bool, setBool] = useState(false);
 
-  const handleName = (e) => {
-    setName(e.target.value);
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handleMessage = (e) => {
-    setMessage(e.target.value);
-  };
   console.log(name);
   const submitForm = async (e) => {
     e.preventDefault();
@@ -46,7 +37,9 @@ export default function ContactMe(props) {
       };
       setBool(true);
       const res = await axios.post(
-        `https://nirbhay-portfolio-backend.vercel.app/contact`,
+        {
+          email: "ishitalibra15@gmail.com",
+        },
         data
       );
       if (name.length === 0 || email.length === 0 || message.length === 0) {
@@ -71,48 +64,66 @@ export default function ContactMe(props) {
     <div className="main-container fade-in" id={props.id || ""}>
       <ScreenHeading subHeading={"Lets Get In Touch"} title={"Contact Me"} />
       <div className="central-form">
-        <div className="col">
-          <h2 className="title">
-            <Typical loop={Infinity} steps={["Get In Touch 📧", 1000]} />
-          </h2>{" "}
-          <a href="https://www.linkedin.com/in/ishita-jain15/">
-                <i className="fa fa-linkedin-square"></i>
-              </a>
-          
-              <a href="https://github.com/ishita152001">
-                <i className="fa fa-github"></i>
-              </a>
-        </div>
-        <div className="back-form">
-          <div className="img-back">
-            <h4>Fill up your details here!</h4>
-            <img src={imgBack} alt="image not found" />
-          </div>
-          <form onSubmit={submitForm}>
-            <p>{banner}</p>
-            <label htmlFor="name">Name</label>
-            <input type="text" onChange={handleName} value={name} />
+        <div
+          className="col"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            fontSize: "15px",
+          }}
+        >
 
-            <label htmlFor="email">Email</label>
-            <input type="email" onChange={handleEmail} value={email} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.5rem"
+            fill="currentColor"
+            viewBox="0 0 512 512"  
 
-            <label htmlFor="message">Message</label>
-            <textarea type="text" onChange={handleMessage} value={message} />
+          >
+            <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" />
+          </svg>
+          <a
+            href="mailto:ishitalibra15@gmail.com"
+            className="text-lg"
+            style={{ color: "#1f2235", textDecoration: "none" }}
+          >
+            &nbsp; ishitalibra15@gmail.com &nbsp;
+          </a>
 
-            <div className="send-btn">
-              <button type="submit">
-                send
-                <i className="fa fa-paper-plane" />
-                {bool ? (
-                  <b className="load">
-                    <img src={load1} alt="image not responding" />
-                  </b>
-                ) : (
-                  ""
-                )}
-              </button>
-            </div>
-          </form>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.5rem"
+            fill="currentColor"
+            viewBox="0 0 512 512"
+          >
+            <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+          </svg>
+          <a
+            href="tel:+917982654489"
+            className="text-lg"
+            style={{ color: "#1f2235", textDecoration: "none" }}
+          >
+           &nbsp;  +91-7982654489 &nbsp;
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.5rem"
+            fill="currentColor"
+            viewBox="0 0 512 512"
+          >
+            <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+          </svg>
+          <a
+            href="https://goo.gl/maps/Xy1ss7YfzkxUKK887"
+            target="_blank"
+            rel="noreferrer"
+            className="text-lg"
+            style={{ color: "#1f2235", textDecoration: "none" }}
+          >
+           &nbsp;  Delhi, India
+          </a>
         </div>
       </div>
       <Footer />
